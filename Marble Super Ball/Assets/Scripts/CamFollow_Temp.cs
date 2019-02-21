aa7f9fa3a95e6e44932d7f57656c633d620330d8
NHambley <nhambley6@gmail.com>
2019-02-21 17:33:01 -0500

    temp

---
+++ Marble Super Ball/Assets/Scripts/CamFollow_Temp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow_Temp : MonoBehaviour
+{
+    public GameObject ball;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.LookAt(ball.transform);
+	}
+}
