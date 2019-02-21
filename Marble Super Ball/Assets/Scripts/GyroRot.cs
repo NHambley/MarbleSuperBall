0bf910ee0bf4483b8c62ee1e38539a5da85e8323
Fredrick Ditondo <fmd8442@g.rit.edu>
2019-02-21 05:38:59 -0500

    Something something rotato

---
+++ Marble Super Ball/Assets/Scripts/GyroRot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroRot : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation *= Quaternion.Euler(Input.acceleration.y / 6, -Input.acceleration.x / 3, 0);
+	}
+}
