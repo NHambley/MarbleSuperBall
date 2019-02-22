c8dda489b988260b9d4536e499090af405994944
Fredrick Ditondo <fmd8442@g.rit.edu>
2019-02-21 20:45:48 -0500

    Something about Quaternions

--- Marble Super Ball/Assets/Scripts/GyroRot.cs
+++ Marble Super Ball/Assets/Scripts/GyroRot.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GyroRot : MonoBehaviour {
-    
+
+    private Vector3 deadZone = Vector3.zero;
+    private Matrix4x4 calibrateMat;
 
 	// Use this for initialization
 	void Start () {
-
+        CalibrateAccel();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.rotation *= Quaternion.Euler(Input.acceleration.y / 6, -Input.acceleration.x / 3, 0);
+        Vector3 accel = calibrateMat.MultiplyVector(Input.acceleration);
+        transform.rotation *= Quaternion.Euler(accel.y,-accel.x, 0);
 	}
+
+    // set deadzone
+    void CalibrateAccel()
+    {
+        deadZone = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0f, 0f, -1f), deadZone);
+        Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, rotateQuaternion, new Vector3(1f, 1f, 1f));
+        calibrateMat = matrix.inverse;
+    }
 }
