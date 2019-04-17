51b8ba5b851ee15a666033cafcaa091c556a36d5
NHambley <nhambley6@gmail.com>
2019-04-17 00:48:30 -0400

    added turotial level 1 and began rework of main menu

---
+++ Marble Super Ball/Assets/Pointer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject endpoint;
+    Vector3 ePos;
+    Vector3 pPos;
+
+    float rotSpeed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        endpoint = GameObject.FindGameObjectWithTag("EndPoint");
+        pPos = transform.position;
+        ePos = endpoint.transform.position;// this doesn't need changed once it's established
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pPos = player.transform.position;
+        Vector3 dir = ePos - pPos;
+        dir = player.transform.InverseTransformDirection(dir);
+        float angleBetween = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotSpeed * angleBetween);
+    }
+}
