e0822e80fd1d493e8dbba71c057d1be99b6fcf79
NHambley <nhambley6@gmail.com>
2019-04-23 18:06:44 -0400

    added sound and tweaked some things

---
+++ Marble Super Ball/Assets/Scripts/SoundPlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+    AudioSource sound;
+    float angV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        angV = GetComponent<Rigidbody>().angularVelocity.magnitude;
+        if (angV > 2.0f)
+            sound.Play();
+        else
+            sound.Stop();
+    }
+}
