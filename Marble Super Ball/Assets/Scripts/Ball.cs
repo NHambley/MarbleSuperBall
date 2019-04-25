c215c28a67d1663b370938796b5bae80870e6a89
Conor Lilley <cql3842@g.rit.edu>
2019-04-25 05:53:44 -0400

    Added in wind zones
    Made a tutorial level for wind
    Updated menu ui
    2 Scripts added
     - WindArea
     - Ball

---
+++ Marble Super Ball/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public bool inWindZone = false;
+    public GameObject windZone;
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (inWindZone)
+        {
+            rb.AddForce(windZone.GetComponent<WindArea>().direction * windZone.GetComponent<WindArea>().strength);
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.tag == "windArea")
+        {
+            windZone = coll.gameObject;
+            inWindZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if(coll.gameObject.tag == "windArea")
+        {
+            inWindZone = false;
+        }
+    }
+}
---
+++ Marble Super Ball/Assets/Scripts/WindArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    //strength of wind
+    public float strength;
+    //direction of wind
+    public Vector3 direction;
+}
