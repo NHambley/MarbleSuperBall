078163d8c59cf1dbdc6cf5038bfd4441d978f72d
Fredrick Ditondo <fmd8442@g.rit.edu>
2019-04-28 13:56:25 -0400

    checkpoint code

---
+++ Marble Super Ball/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject respawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            respawnPoint.transform.position = this.transform.position;
+        }
+    }
+}
