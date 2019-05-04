e27f2ff4ba3b7064c5cede9ecb5539d6c6a18712
James Schrupp (RIT Student) <jds5583@ad.rit.edu>
2019-05-04 16:28:08 -0400

    Gate
    Need to set next level and need to add to build settings.

---
+++ Marble Super Ball/Assets/Scripts/GateButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateButton : MonoBehaviour
+{
+    Renderer rend;
+    public Material[] material;
+    [HideInInspector]
+    public bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //Set to active, lower button to appear flattened.
+            transform.position.Set(transform.position.x, transform.position.y - 0.1f, transform.position.z);
+            activated = true;
+        }
+    }
+
+   
+}
---
+++ Marble Super Ball/Assets/Scripts/GateControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateControl : MonoBehaviour
+{
+    //On and Off Materials
+    public Material[] material;
+    //Gate Lights
+    public GameObject[] Lights;
+    //Gate Buttons
+    public GameObject[] Buttons;
+
+    //Active Buttons Tracker
+    public bool[] ActiveButtons;
+    //Active Lights (Not tied to specific Buttons)
+    int activeLights;
+    Renderer rend;
+    //Gate Door and desired end position
+    public GameObject Door;
+    private float doorEndPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeLights = 0;
+        doorEndPosition = Door.transform.position.y - 8;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            //If Button was inactive, check.
+            if (ActiveButtons[i] == false)
+            {
+                if (Buttons[i].GetComponent<GateButton>().activated == true)
+                {
+                    //Light up Button
+                    rend = Buttons[i].GetComponent<Renderer>();
+                    rend.enabled = true;
+                    rend.sharedMaterial = material[1];
+                    //set Button to active in ActiveButtons
+                    ActiveButtons[i] = true;
+                    //Set next light to active;
+                    activeLights++;
+                    //Light up next Light
+                    rend = Lights[activeLights - 1].GetComponent<Renderer>();
+                    rend.enabled = true;
+                    rend.sharedMaterial = material[1];
+                }
+            }
+        }
+        //If all lights active, and door exists(Not fully opened), Open door
+        if(activeLights == Lights.Length)
+        {
+            if (Door)
+            {
+                Open();
+            }
+        }
+
+    }
+
+    void Open()
+    {
+        //While Door is above end position
+        if(Door.transform.position.y > doorEndPosition)
+        {
+            Door.transform.Translate(-transform.up * Time.deltaTime);
+        }
+    }
+}
